8347f44fb23c60f3924797d15cda3d21cb23c1df
vishnu-ajit <heart.beats5717@gmail.com>
2015-04-06 20:30:24 +0530

    adding first sequence of files and folders

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Buns &amp; Rolls</title>
+<style>
+body
+{
+background-image:url(images/bg-red.jpg);
+background-repeat:no-repeat;
+padding:0;
+margin:0;	
+}
+
+#container
+{
+	margin-left:150px;
+	margin-right:150px;
+	margin-top:20px;
+	margin-bottom:20px;
+	
+	background-color:rgba(192,192,192,0.2);
+}
+</style>
+</head>
+
+<body>
+
+    <div id="container" >
+
+        <?php require 'menu-bar.php'; ?>
+    </div><!--container-->
+
+
+
+</body>
+</html>
---
+++ menu-bar.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mc Donalds India</title>
+</head>
+<style>
+#wrapper
+{
+
+	overflow:auto;
+	
+}
+#left
+{
+	float:left;
+
+}
+#right
+{
+	
+	float:right;
+	font-family:Arial, Helvetica, sans-serif;
+	text-align:right;
+	color:#fff;
+
+}
+
+</style>
+<body>
+
+
+<div id="wrapper">
+    	<div id="left">
+    	        <img src="images/logo.png"></img>
+        </div>
+        <div id="right">
+				
+                <font size=2>
+                    ABOUT US &nbsp;
+                    LOCATE US &nbsp;
+                    CONTACT US
+                </font>
+                <br>
+				<font size="6">             
+                
+		            Call Us 66 000 666
+                </font>
+                <br>
+				<font size="2">
+					Order Online mcdelivery.co.in
+				</font>
+
+        </div>
+	</div>
+</body>
+</html>
